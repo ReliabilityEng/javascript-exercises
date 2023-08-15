ac606a0d66f76b56cb51f9d17d7acb226a2c85eb
ReliabilityEng <isaacmortera@gmail.com>
2023-08-15 19:08:46 +1200

    Modify reverseString function to complete the exercise. Found that chaining method is a better one liner instead of declaring three different variables to do the same action

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function(stringToReverse) {
+    let stringArray = stringToReverse.split('');
+    let arrayReversed = stringArray.reverse();
+    let stringReversed = arrayReversed.join('');
 
+    return stringReversed;
+    // Better solution is to chain methods
+    // return string.split("").reverse().join("");
 };
+
+reverseString('Hello');
 
 // Do not edit below this line
 module.exports = reverseString;
