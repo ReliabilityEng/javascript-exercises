fbae0f63ba47a86c205baaf10dddadf4605e332e
ReliabilityEng <isaacmortera@gmail.com>
2023-08-15 18:57:43 +1200

    Modify repeatString function to comply with the test cases. Removed skip on the spec.js to run the rest of the test to finish exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(textToRepeat, repetitionCount) {
+    
+    if (repetitionCount < 0) {
+        return 'ERROR';
+    } else {
+        let myString = '';
+        for (let i = 0; i < repetitionCount; i++) {
+            myString+=textToRepeat;
+        }
+        return myString;
+    }
 };
 
 // Do not edit below this line
