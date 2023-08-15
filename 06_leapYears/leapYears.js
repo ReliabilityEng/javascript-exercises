432223a27c4d65c7d1cd8f1f5b04e5a3299dd502
ReliabilityEng <isaacmortera@gmail.com>
2023-08-15 22:43:31 +1200

    Add the ternary operator to leapYears function attempt 2 to write attempt1 in a more efficient way

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,29 @@
-const leapYears = function() {
+// // Attempt1
+// const leapYears = function(year) {
+//     if(year % 4 === 0){
+//         if(year % 100 === 0){
+//             if (year % 400 === 0) {
+//                 return true;
+//             } else {
+//                 return false;
+//             }
+//         } else {
+//             return true;
+//         }
+//     } else {
+//         return false;
+//     }
+// };
 
+// Attempt2
+const leapYears = function(year) {
+    const isleapYear = 
+    year % 4 === 0 && year % 100 !== 0 ? true : 
+    year % 100 === 0 && year % 400 === 0 ? true : false;
+
+    return isleapYear;
 };
+
 
 // Do not edit below this line
 module.exports = leapYears;
