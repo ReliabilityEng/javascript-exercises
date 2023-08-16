042a03ff5f04b430ee311858956fc8e81707f308
ReliabilityEng <isaacmortera@gmail.com>
2023-08-16 18:28:12 +1200

    Modify the sumAll function to achieve the same result with less lines of code Used the isInteger method of number instead to shorten the validity of input. Used destructuring technique to swap values between 2 variables. Different approach with the for loop, using a simple sum instead of the shift array method

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,41 +1,15 @@
 const sumAll = function(num1, num2) {
-    // Check validity of input
-    // Cannot be negatives or non numerical
-    if (typeof(num1) === 'string' || num1 < 0 || isNaN(num1) ||
-        typeof(num2) === 'string' || num2 < 0 || isNaN(num2))
-    {
-        return 'ERROR';
+    // Identify incorrect inputs
+    if (!(Number.isInteger(num1) && Number.isInteger(num2))) return 'ERROR';
+    if (num1 < 0 || num2 < 0) return 'ERROR';
+    if (num1 > num2) [num1, num2] = [num2, num1];
+
+    // Ensure that num1 < num2
+    let sum = 0;
+    for (let i = num1; i <= num2; i++) {
+        sum += i;
     }
-
-    // Identify which of the num1, num2 arguments is lower
-    // This is used to set startNum and endNum for increasing order
-    const identifyLowerNum = (num1, num2) => {
-    if (num1 > num2) {
-        const startNum = num2;
-        const endNum = num1;
-        return {'startNum':startNum, 'endNum': endNum};
-    } else {
-        const startNum = num1;
-        const endNum = num2;
-        return {'startNum':startNum, 'endNum': endNum};
-    }
-    }
-
-    const startNum = identifyLowerNum(num1, num2).startNum;
-    const endNum = identifyLowerNum(num1, num2).endNum;
-    
-    // Generating the array of sum queue to add
-    // +1 added to numDelta to get the correct array size
-        const arraySize = endNum - startNum + 1;
-        const sumQueue = [...Array(arraySize).keys()].map((element) => element + startNum);
-
-    // Adding all the element in array
-        let sum = 0;
-        for (let i = 0; i < arraySize; i++){
-            sum += sumQueue.shift();
-        }
-
-        return sum;
+    return sum;
 };
 
 // Do not edit below this line
