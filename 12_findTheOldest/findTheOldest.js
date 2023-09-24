d8c1baf9e96e7092402e6d91ef6c20425e715e16
ReliabilityEng <isaacmortera@gmail.com>
2023-09-25 09:21:19 +1300

    Finish exercise as required

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArray) {
+    // Calculate each person's age - modify array
+    peopleArray.map((person) => {
+        if(!person.yearOfDeath) {
+            person.age = 2023 - person.yearOfBirth;
+        } else {
+            person.age = person.yearOfDeath - person.yearOfBirth;
+        }
+    });
 
+    ageSorted = peopleArray.sort((person1, person2) => {
+        return person1.age > person2.age ? -1 : 1;
+    });
+
+    oldestPerson = ageSorted[0];
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
