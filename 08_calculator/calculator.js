7c00c6b01d0487c50771d71464df9fa22e90cd71
ReliabilityEng <isaacmortera@gmail.com>
2023-09-24 22:40:45 +1300

    Finish the exercise as required.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,26 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(myArray) {
+	return myArray.reduce((total, elem) => total += elem, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(myArray) {
+  return myArray.reduce((total, elem) => total *= elem, 1);
 };
 
-const power = function() {
-	
+const power = function(num1,num2) {
+	return num1**num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  factorialArray = [...Array(num).keys()].map(num => num + 1);
+	return factorialArray.reduce((product, num) => product *= num, 1);
 };
 
 // Do not edit below this line
