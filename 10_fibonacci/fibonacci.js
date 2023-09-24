19ca10b54877490432a8d7634a38de70329452fd
ReliabilityEng <isaacmortera@gmail.com>
2023-09-24 23:40:05 +1300

    Finish exercise as required.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(fibonnaciIndex) {
+    myArray = [];
+    if(fibonnaciIndex < 0) return 'OOPS';
 
+    for(let i = 0; i < fibonnaciIndex; i++) {
+        if(i === 0 || i === 1){
+            myArray[i] = 1;
+        } else {
+            myArray[i] = myArray[i - 1] + myArray[i - 2];
+        }
+    }
+    return myArray[myArray.length - 1];
 };
 
 // Do not edit below this line
