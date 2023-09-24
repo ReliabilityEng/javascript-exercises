d85e4729adc7059a6c8d3744e22b24dda7e30fd3
ReliabilityEng <isaacmortera@gmail.com>
2023-09-24 23:06:51 +1300

    Finish the exercise as required

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (myString) {
+    let pattern = [...myString.replace(/[^a-z0-9]/gi, '').toLowerCase()];
+    let patternReverse = Array.from(pattern).reverse();
+    return pattern.join('') === patternReverse.join('');
 };
 
 // Do not edit below this line
